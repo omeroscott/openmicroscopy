fd8056aa9cecbe17a59e21646978e9b76a558452
jmoore <josh@glencoesoftware.com>
2011-06-22 14:18:02 +0200

    Merging omero.util.hic_parser into omero_hic and fixing lint/flake issues

--- components/tools/OmeroPy/src/omero_hic.py
+++ components/tools/OmeroPy/src/omero_hic.py
@@ -7,19 +7,41 @@
 #
 
 """
-Conversion from SQL to OMERO.tables actions.
+Tools for the OMERO/HIC project.
+
+Includes:
+
+ * Conversion from SQL to OMERO.tables queries.
+ * Conversion from Schema files to OMERO.tables.
+
 """
 
+import datetime
+import logging
 import numpy
+import os
+import StringIO
+import sys
 import tables
 import tempfile
+import time
 import traceback
 import unittest
 
+try:
+    from xml.etree.ElementTree import XML
+    assert XML # Workaround for PyFlakes
+except ImportError:
+    from elementtree.ElementTree import XML
+
 import sqlparse
 
-from sqlparse.sql import IdentifierList, Identifier, Where, Parenthesis
-from sqlparse.tokens import DML, Keyword, Newline, Punctuation, Whitespace, Wildcard
+from sqlparse.sql import IdentifierList, Identifier, \
+        Where, Parenthesis
+from sqlparse.tokens import DML, Keyword, Newline, \
+        Punctuation, Whitespace, Wildcard
+
+from omero.grid import LongColumn, StringColumn
 
 
 SQL_TEMPLATE = """
@@ -32,6 +54,7 @@ SQL_TEMPLATE = """
             OFFSET %(offset)s
 """
 
+
 class Aggregate(object):
 
     def __init__(self, func, contents):
@@ -39,13 +62,24 @@ class Aggregate(object):
         self.contents = contents
 
     def to_unicode(self):
-        return "%s(%s)" % (self.func.to_unicode(), ", ".join([x.to_unicode() for x in self.contents]))
+        return "%s(%s)" % (self.func.to_unicode(),\
+                ", ".join([x.to_unicode() for x in self.contents]))
 
 
-class ParserException(Exception): pass
-class Unsupported(ParserException): pass
-class Internal(ParserException): pass
-class Invalid(ParserException): pass
+class ParserException(Exception):
+    pass
+
+
+class Unsupported(ParserException):
+    pass
+
+
+class Internal(ParserException):
+    pass
+
+
+class Invalid(ParserException):
+    pass
 
 
 class Parser(object):
@@ -54,7 +88,8 @@ class Parser(object):
         self.sql = sql
         self.parsed = sqlparse.parse(sql)
         if len(self.parsed) != 1:
-            raise Unsupported("Wrong number of statements: %s", len(self.parsed))
+            raise Unsupported("Wrong number of statements: %s",\
+                    len(self.parsed))
         self.statement = self.parsed[0]
         self.all_tokens = self.statement.tokens
 
@@ -78,7 +113,7 @@ class Parser(object):
                 elif len(_from) != 1:
                     raise Unsupported("Too many tables for count: %s", self)
                 else:
-                    target =  _from[0].get_name()
+                    target = _from[0].get_name()
                     return source.nrows(target)
         # No aggregates
         assert not self.parts["where"]
@@ -100,7 +135,7 @@ class Parser(object):
                 try:
                     reverse_column_names[column_name].append(table_name)
                 except KeyError:
-                    reverse_column_names = {column_name:[table_name]}
+                    reverse_column_names = {column_name: [table_name]}
 
         wc_token_lists = (self.parts["select"], )
         nowc_token_lists = (self.parts["where"], self.parts["groupby"], self.parts["orderby"])
@@ -139,9 +174,10 @@ class Parser(object):
                         else:
                             value = token.tokens[0].to_unicode()
                             try:
-                                tables = reverse_column_names[value]
-                                if len(tables) != 1:
-                                    raise Invalid("Non-unique column name: %s (%s)" % (value, ", ".join(tables)))
+                                table_names = reverse_column_names[value]
+                                if len(table_names) != 1:
+                                    raise Invalid("Non-unique column name: %s \
+                                            (%s)" % (value, ", ".join(table_names)))
                             except KeyError:
                                 raise Invalid("Unknown column: %s", value)
 
@@ -152,23 +188,23 @@ class Parser(object):
         rv = """Parser(sql='%s',
             %s""" % (self.sql, SQL_TEMPLATE)
         return rv % dict([(k, " ".join([x.to_unicode() for x in v])) for k, v in self.parts.iteritems()])
-
-    ##
-    ## Definers
-    ##
-
+##
+##
+##  Definers
+##
+##
     def define_parts(self):
         """
         Split into: %s
         """ % SQL_TEMPLATE
         self.parts = {
-            "select":[],
-            "from":[],
-            "where":[],
-            "groupby":[],
-            "orderby":[],
-            "limit":[],
-            "offset":[],
+            "select": [],
+            "from": [],
+            "where": [],
+            "groupby": [],
+            "orderby": [],
+            "limit": [],
+            "offset": [],
         }
 
         current = None
@@ -221,15 +257,14 @@ class Parser(object):
             else:
                 current.append(token)
 
-
     def define_tables(self):
         stream = self.extract_from_part(self.statement)
         self.tables = list(self.extract_table_identifiers(stream))
-
-    ##
-    ## Keyword/clause detection methods
-    ##
-
+##
+##
+##  Keyword/clause detection methods
+##
+###
     def skip(self, token):
         return token.ttype is Whitespace or token.ttype is Newline or token.ttype is Punctuation
 
@@ -366,70 +401,72 @@ class Test(unittest.TestCase):
         m = {"select": 1, "from": 1}
         self.assertParts(p.parts, **m)
 
-    # With HDF Files
+    """
+    With HDF Files
+    """
 
     def testSimpleCount(self):
         p = Parser("select count(*) from bp")
         self.assertEquals(["bp"], p.tables)
         source = self.data({"bp": {"a": tables.Int64Col()}})
-        source.add("bp", {"a":[(1,),(2,),(3,)]})
+        source.add("bp", {"a": [(1, ), (2, ), (3, )]})
         result = p.execute(source)
         self.assertEquals([[3]], result)
 
     def testSimpleCountWithGrouping(self):
         p = Parser("select a, count(*) from bp group by a")
         source = self.data({"bp": {"a": tables.Int64Col()}})
-        source.add("bp", {"a":[(1,),(2,),(3,)]})
+        source.add("bp", {"a": [(1, ), (2, ), (3, )]})
         self.assertRaises(Unsupported, p.execute, source)
 
     def testColumnsInTablePass(self):
         p = Parser("select a from bp")
         source = self.data({"bp": {"a": tables.Int64Col()}})
-        source.add("bp", {"a":[(1,),(2,)]})
+        source.add("bp", {"a": [(1, ), (2, )]})
         result = p.execute(source)
-        self.assertEquals([(1,), (2,)], result)
+        self.assertEquals([(1, ), (2, )], result)
 
     def testColumnsInTableFail(self):
         p = Parser("select MISSING from bp")
         source = self.data({"bp": {"a": tables.Int64Col()}})
-        source.add("bp", {"a":[(1,),(2,)]})
+        source.add("bp", {"a": [(1, ), (2, )]})
         self.assertRaises(Invalid, p.execute, source)
 
     def testWildcard(self):
         p = Parser("select * from bp")
         a = tables.Int64Col(pos=0)
         b = tables.Int64Col(pos=1)
-        source = self.data({"bp": {"a":a, "b":b}})
+        source = self.data({"bp": {"a": a, "b": b}})
 
-        a = [(1,),(2,)]
-        b = [(3,),(4,)]
-        source.add("bp", {"a":a, "b":b})
+        a = [(1, ), (2, )]
+        b = [(3, ), (4, )]
+        source.add("bp", {"a": a, "b": b})
         result = p.execute(source)
-        self.assertEquals([(1,3), (2,4)], result)
+        self.assertEquals([(1, 3), (2, 4)], result)
 
     def testSpeciicWildcard(self):
         p = Parser("select bp.* from bp")
         a = tables.Int64Col(pos=0)
         b = tables.Int64Col(pos=1)
-        source = self.data({"bp": {"a":a, "b":b}})
+        source = self.data({"bp": {"a": a, "b": b}})
 
-        a = [(1,),(2,)]
-        b = [(3,),(4,)]
-        source.add("bp", {"a":a, "b":b})
+        a = [(1, ), (2, )]
+        b = [(3, ), (4, )]
+        source.add("bp", {"a": a, "b": b})
         result = p.execute(source)
-        self.assertEquals([(1,3), (2,4)], result)
+        self.assertEquals([(1, 3), (2, 4)], result)
 
     def testAliasWildcard(self):
         p = Parser("select bp.* from bp_long_name as bp ")
         a = tables.Int64Col(pos=0)
         b = tables.Int64Col(pos=1)
-        source = self.data({"bp_long_name": {"a":a, "b":b}})
+        source = self.data({"bp_long_name": {"a": a, "b": b}})
 
-        a = [(1,),(2,)]
-        b = [(3,),(4,)]
-        source.add("bp_long_name", {"a":a, "b":b})
+        a = [(1, ), (2, )]
+        b = [(3, ), (4, )]
+        source.add("bp_long_name", {"a": a, "b": b})
         result = p.execute(source)
-        self.assertEquals([(1,3), (2,4)], result)
+        self.assertEquals([(1, 3), (2, 4)], result)
 
     def assertParts(self, parts, **kwargs):
         for k, v in kwargs.iteritems():
@@ -450,8 +487,214 @@ class Test(unittest.TestCase):
     def testFromAliasInWildCardSelect(self):
     """
 
+
+class Column(object):
+    """
+    Single element from XPath /schema/columns/column
+    Attributes and subelements are added as fields
+    """
+
+    def __init__(self, element = None, **kwargs):
+        if element is not None:
+            self.pos = element.attrib["pos"]
+            for x in ("name", "type", "descriptor"):
+                setattr(self, x, element.find(x).text)
+        for k, v in kwargs.iteritems():
+            setattr(self, k, v)
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        return "%s(pos='%s', name='%s', type='%s', descriptor='%s')" %\
+            (self.__class__.__name__,\
+            self.pos,\
+            self.name and self.name or "",\
+            self.type and self.type or "",\
+            self.descriptor and self.descriptor or "")
+
+
+class SchemaXml(object):
+    """
+
+    """
+    attributes = ("filename", "datecreated", "linecount", "filesize",\
+            ("separator", "|"), "dateformat", "descriptor", "comment")
+
+    def __init__(self, file = None, **kwargs):
+        self.logger = logging.getLogger(self.__class__.__name__)    #: Logs to the class name
+
+        if file is not None:
+            if isinstance(file, basestring):
+                self.source = open(file, "r")   #: Open file handle
+            else:
+                self.source = file              #: StringIO for testing
+
+            self.text = self.source.read()
+            self.XML = XML(self.text)
+            self.summary = self.XML.find("summary")
+            for x in SchemaXml.attributes:
+                default = None
+                if isinstance(x, tuple):
+                    x, default = x # Unwrap defaults
+                val = self.summary.find(x).text
+                if default and not val:
+                    val = default
+                setattr(self, x, val)
+            self.columns = [Column(col) for col in self.XML.find("columns").findall("./column")]
+        for k, v in kwargs.iteritems():
+            setattr(self, k, v)
+
+    def map_dtype(self, dtype):
+        kind = dtype.kind
+        if kind == "S":
+            return StringColumn()
+        elif kind == "O":
+            # Assuming date
+            return LongColumn()
+        elif kind == "i":
+            return LongColumn()
+        else:
+            raise Exception(kind)
+
+    def map_schema(self, type):
+        #
+        # varchar, char, date, int
+        #
+        raise Exception(type)
+
+    def check_dtype(self, col, dtype):
+        assert isinstance(self.map_dtype(dtype), col.__class__)
+
+    def check_schema(self, col, type):
+        if not type:
+            return
+        elif type in ("varchar", "char"):
+            assert isinstance(col, StringColumn)
+        elif type == "date":
+            assert isinstance(col, LongColumn)
+        elif type == "int":
+            assert isinstance(col, LongColumn)
+        else:
+            raise Exception(type)
+
+    def initialize(self, table, data = None):
+        from matplotlib.mlab import csv2rec
+        cs = None
+        if data:
+            for csv in data:
+                records = csv2rec(csv, delimiter=self.separator)
+                types = records.dtype
+                if cs is None:
+                    cs = [self.map_dtype(x) for x in [types[i] for i in range(len(types))]]
+                else:
+                    if len(types) != len(cs):
+                        raise Exception("len(types)==%1 <> len(columns)==%1" % (len(types), len(cs)))
+                    for idx, type in enumerate(types):
+                        self.check_dtype(cs[idx], type)
+
+        if cs is None:
+            cs = [self.map_schema(col.type) for col in self.columns]
+        else:
+            for idx, col in enumerate(self.columns):
+                self.check_schema(cs[idx], col.type)
+
+        # Handle name, etc
+        for idx, col in enumerate(self.columns):
+            c = cs[idx]
+            c.name = col.name
+            c.description = col.descriptor
+            if isinstance(c, StringColumn):
+                c.size = 100
+
+        table.initialize(cs)
+
+        def epoch(dt):
+            return long(time.mktime(dt.timetuple())*1000)
+
+        if data:
+            for csv in data:
+                cs = table.getHeaders()
+                records = csv2rec(csv, delimiter=self.separator)
+                dtype = records.dtype
+                for x in range(len(dtype)):
+                    if isinstance(records[dtype.names[x]][0], datetime.date): # FIXME
+                        cs[x].values = [epoch(dt) for dt in records[dtype.names[x]]]
+                    else:
+                        cs[x].values = records[dtype.names[x]].tolist()
+                table.addData(cs)
+
+        return cs
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        contents = ""
+        for attribute in SchemaXml.attributes:
+            default = ""
+            if isinstance(attribute, tuple):
+                attribute = attribute[0]
+                default = attribute[1]
+        contents += "  %s = '%s',\n" % (attribute, getattr(self, attribute, default))
+        contents += "  columns = [\n"
+        contents += ",\n".join(["    " + (x and str(x) or "") for x in self.columns])
+        contents += "\n  ]\n"
+        return """%s(
+%s)""" % (self.__class__.__name__, contents)
+
+
+class SchemaTest(unittest.TestCase):
+
+    def testPrint(self):
+        sio = StringIO.StringIO("""<schema>
+  <summary>
+    <filename>mar10_267_BP.txt</filename>
+    <datecreated>31/03/2010</datecreated>
+    <linecount>468,622</linecount>
+    <filesize>26,445,780</filesize>
+    <separator />
+    <dateformat />
+    <descriptor />
+    <comment />
+  </summary>
+  <columns>
+    <column pos="0">
+      <name>prochi</name>
+      <type />
+      <descriptor />
+    </column>
+    <column pos="1">
+      <name>bp_date</name>
+      <type />
+      <descriptor />
+    </column>
+    <column pos="2">
+      <name>SBP</name>
+      <type />
+      <descriptor />
+    </column>
+    <column pos="3">
+      <name>DBP</name>
+      <type />
+      <descriptor />
+    </column>
+    <column pos="4">
+      <name>DataSource_ID</name>
+      <type />
+      <descriptor />
+    </column>
+    <column pos="5">
+      <name>DataSource_Description</name>
+      <type />
+      <descriptor />
+    </column>
+  </columns>
+</schema>""")
+        print SchemaXml(sio)
+
+
 if __name__ == '__main__':
-    unittest.main()
     if "TRACE" in os.environ:
         import trace
         tracer = trace.Trace(ignoredirs=[sys.prefix, sys.exec_prefix], trace=1)
